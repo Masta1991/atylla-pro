25d0a37951ac0f7233c7d2aac6616ccaa7636e55
Masta1991 <97475976+Masta1991@users.noreply.github.com>
2026-05-29 11:26:01 +0200

    v1.0.25 - global cache optimization + PWA build

--- backend/static/sw.js
+++ backend/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'atylla-pro-v1.0.24d';
+const CACHE_NAME = 'atylla-pro-v1.0.25';
 const urlsToCache = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', event => {
